c783107936880c9920987d583332812c7eddcddc
rns-joshua <joshua@rednovasolutions.com>
2025-10-31 09:25:40 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 #If GAM is not installed start setup or otherwise just run gam
 if [ ! -d "$HOME/bin/gam" ]; then
-    $HOME/install-gam
+    /tmp/install-gam
 elif [[ "$@" == "update-gam" ]]; then
     bash <(curl -s -S -L https://git.io/install-gam) -l
 else
